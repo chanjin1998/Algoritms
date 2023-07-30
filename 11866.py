8dcc8175c4df44083fceca991b53df329f44df34
chanjin1998 <chanjin@kookmin.ac.kr>
2023-07-30 20:23:51 +0900

    Math : SilverV : 11866

--- 11866.py
+++ 11866.py
@@ -1,11 +1,43 @@
+# import sys
+# n,k = map(int,sys.stdin.readline().split())
+# circle = [i for i in range(1,n+1)]
+# res = []
+# cnt = k
+# while (1):
+#     if len(circle) == 0:
+#         break
+#     elif len(circle) == 2:
+#         res.append(circle[0])
+#         res.append(circle[1])
+#         circle.remove(circle[0])
+#         circle.remove(circle[0])
+#     elif cnt > len(circle):
+#         cnt = cnt%len(circle)
+#         res.append(circle[cnt-1])
+#         circle.remove(circle[cnt-1])
+#         cnt += 2
+#     else :
+#         res.append(circle[cnt-1])
+#         circle.remove(circle[cnt-1])
+#         cnt += 2
+# print('<',end='')
+# for i in range(len(res)):
+#     if i == len(res)-1:
+#         print(res[i],end='')
+#     else:
+#         print(res[i],end=', ')
+# print('>')
 import sys
-n,k = map(int,sys.stdin.readline().split())
-circle = [i for i in range(1,n+1)]
+
+n, k = map(int, sys.stdin.readline().split())
+
+idx = 0
+queue = [i for i in range(1, n+1)]
 res = []
-while (1):
-    if len(circle) == 0:
-        break
-    res.append(circle[(k-1)%len(circle)])
-    del circle[(k-1)%len(circle)]
-    k += 3
-print(res)
+while queue:
+    idx += k - 1
+    if idx >= len(queue):
+        idx %= len(queue)
+    res.append(str(queue.pop(idx)))
+
+print("<", ", ".join(res), ">", sep="")
