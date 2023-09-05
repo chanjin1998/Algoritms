a574a91e79b3e85b60d2aa2500746dda8782f5b6
chanjin1998 <chanjin@kookmin.ac.kr>
2023-09-05 17:04:59 +0900

    math : Bronze3 : 2455

---
+++ 2455.py
@@ -0,0 +1,12 @@
+import sys
+
+arr = []
+result = 0
+
+for i in range(4):
+    a, b = map(int,sys.stdin.readline().split())   # a와 b 입력 4번씩 받기
+    
+    result = result - a + b
+    arr.append(result)
+        
+print(max(arr))
