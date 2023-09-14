91127419f89ca579d92fcaee2f878dad3531f65b
chanjin1998 <chanjin@kookmin.ac.kr>
2023-09-14 14:35:28 +0900

    graph : GoldV : 7576

---
+++ 7576.py
@@ -0,0 +1,46 @@
+from collections import deque
+import sys
+
+def bfs():
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0 and visited[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                visited[nx][ny] = 1
+                q.append((nx, ny))
+
+
+m, n = map(int, sys.stdin.readline().split())
+graph = []
+visited = [[0 for j in range(m)] for i in range(n)]
+
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().split())))
+
+q = deque()
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            q.append((i, j))
+
+bfs()
+max_cnt = 0
+for i in range(len(graph)):
+    if 0 in graph[i]:
+        answer = -1
+        break
+    else:
+        max_num = max(graph[i])
+        if max_num > max_cnt:
+            max_cnt = max_num
+print(max_cnt - 1)
