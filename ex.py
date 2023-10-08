583afb613257f0b1850c9618bdf801958ad8a1e8
chanjin1998 <chanjin@kookmin.ac.kr>
2023-10-08 23:13:00 +0900

    ex upload

--- ex.py
+++ ex.py
@@ -1,20 +1,4 @@
-from cmath import sqrt
 import sys
-import math
-n = int(sys.stdin.readline())
-a = int(pow(n,1/2))
+a,b = map(int,sys.stdin.readline().split())
 
-val = []
-for i in range(a,0,-1):
-  ans = []
-  res = 0
-  while (True):
-    if n == res:
-      break
-    if n-res >= pow(i,2):
-      res += pow(i,2)
-      ans.append(i)
-    else:
-      i -= 1
-  val.append(len(ans))
-print(min(val))
+print(a*(b-1)+1)
