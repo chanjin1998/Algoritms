4d652c1e492852bb544c433b324eaf008b6e6755
정찬진 <chanjin@kookmin.ac.kr>
2023-10-22 19:32:56 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,4 @@
+-- 코드를 입력하세요
+SELECT member_id, member_name, gender, date_format(date_of_birth,'%Y-%m-%d') as date_of_birth from member_profile
+where gender = 'W' and month(date_of_birth) = 03 and tlno is not null
+order by member_id
