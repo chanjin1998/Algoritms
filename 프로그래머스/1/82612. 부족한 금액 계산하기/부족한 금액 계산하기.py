b03539bad18c793644990170d191cbbc859d3efb
정찬진 <chanjin@kookmin.ac.kr>
2023-11-03 15:38:50 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,9 @@
+def solution(price, money, count):
+    answer = -1
+    total_price = 0
+    for i in range(1,count+1):
+        total_price += price * i
+    if total_price > money:
+        return total_price - money
+    else:
+        return 0
