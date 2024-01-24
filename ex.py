4c52fed2e362c9532fd4ad40b9a12764154d30a3
chanjin1998 <chanjin@kookmin.ac.kr>
2024-01-25 08:24:22 +0900

    7402.py

--- ex.py
+++ ex.py
@@ -1,32 +1,46 @@
 import sys
-n,c = map(int,sys.stdin.readline().split())
-location = []
-visited = []
-for i in range(n):
-  if i == 0 or i == (n-1):
-    visited.append(1)
-    location.append(int(sys.stdin.readline()))
+import heapq
+
+min_heap = []
+max_heap = []
+cnt_i = 0
+cnt_d = 0
+N = int(sys.stdin.readline())
+for _ in range(N):
+  T = int(sys.stdin.readline())
+  cnt_i = 0
+  cnt_d = 0
+  for _ in range(T):
+    cal, num = sys.stdin.readline().split()
+    num = int(num)
+    if cal == 'I':
+      cnt_i += 1
+      heapq.heappush(min_heap, num)
+      heapq.heappush(max_heap,(-num,num))
+    else:
+      cnt_d += 1
+      if num == 1:
+        if len(max_heap) == 0:
+          pass
+        else:
+          
+          heapq.heappop(min_heap)
+          min_heap.clear()
+          for i in max_heap:
+            heapq.heappush(min_heap,i[1])
+      elif num == -1:
+        if len(min_heap) ==0:
+          pass
+        else:
+          heapq.heappop(min_heap)
+          min_heap.clear()
+          for i in max_heap:
+            heapq.heappush(min_heap,i[1])
+
+      if (cnt_i - cnt_d) <= 0:
+        min_heap.clear()
+        max_heap.clear()
+  if (cnt_i - cnt_d) <= 0:
+      print('EMPTY')
   else:
-    visited.append(0)
-    location.append(int(sys.stdin.readline()))
-
-location.sort()
-
-cnt = 2
-tmp[0].append(location[0])
-tmp[n-1].append(location[n-1])
-start = 0
-end = n - 1
-temp = 0
-while (cnt<c):
-  tmp = [[n] for _ in range(n)]
-
-  for i in range(1,n-1):
-    if visited[i] == 0:
-      tmp[i].append(min(location[i] - location[start]),(location[end] - location[i]))
-  
-  tmp = sorted(tmp, key=lambda x: x[1])
-  temp = tmp[-1][1]
-  cnt += 1
-print(temp)
-  
+      print(heapq.heappop(max_heap)[1],heapq.heappop(min_heap))
