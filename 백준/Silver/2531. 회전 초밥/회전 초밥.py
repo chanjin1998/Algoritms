3c52c5a6b96d8aec52f92f7334849748a7135b2c
정찬진 <chanjin@kookmin.ac.kr>
2024-03-17 23:47:30 +0900

    [Silver I] Title: 회전 초밥, Time: 3004 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,17 @@
+import sys
+n,d,k,c = map(int,sys.stdin.readline().split())
+plate = []
+max_sushi = 0
+for _ in range(n):
+  plate.append(int(sys.stdin.readline()))
+for i in range(n):
+    if i <= n-k:
+        tmp = set(plate[i:i+k])
+    else:
+        tmp = set(plate[i:])
+        tmp.update(plate[:(i+k)%n])  # %나머지 기호를 잘 사용하자!
+    #print(tmp, end=' ')
+    tmp.add(c)
+    #print(tmp)
+    max_sushi = max(max_sushi, len(tmp))
+print(max_sushi)
