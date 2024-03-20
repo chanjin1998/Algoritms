05c73d972a7ae16e761f8c9b5c6c2c380c4c3757
chanjin1998 <chanjin@kookmin.ac.kr>
2024-03-20 17:32:43 +0900

    .

--- 1747.py
+++ 1747.py
@@ -1,17 +1,48 @@
 import sys
+n,k = map(int,sys.stdin.readline().split())
+num = list(map(int,sys.stdin.readline().split()))
+tmp = list(set(num))
+dic = dict.fromkeys(tmp,0)
 
-N = int(sys.stdin.readline())
-num = []
-res = 0
-temp = 0
-for _ in range(N):
-  tmp = list(map(int,sys.stdin.readline().split()))
-  num.append(tmp)
-num.sort(key=lambda x: (x[1],x[0]))
+start = 0
+end = 0
+dic[num[start]] = 1
+res = []
+while end< n-1:
+  if start <= end:
+    end += 1
+    # print('end:',end)
+    dic[num[end]] += 1
+  while max(dic.values()) > k:
+    res.append(sum(dic.values())-1)
+    dic[num[start]] -= 1
+    # print(dic)
+    start += 1
+res.append(sum(dic.values()))
+print(max(res))
 
-for n in num:
-  if temp <= n[0]:
-    temp = n[1]
-    res += 1
-    
-print(res)
+# import sys
+# input=sys.stdin.readline
+
+# N,K=map(int,input().split())
+
+# L=list(map(int,input().split()))
+
+# start=0 ; end=1
+
+# A=[0]*100001
+
+# A[L[start]]+=1 ; count=1 ; answer=0
+# while end<N:
+#     if A[L[end]]<K:
+#         A[L[end]]+=1
+#         count+=1
+#         end+=1
+
+#     elif A[L[end]]>=K:
+#         A[L[start]]-=1
+#         start+=1
+#         count-=1
+#     answer=max(answer,count)
+
+# print(answer)
