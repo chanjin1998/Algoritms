16d9eafefb593733c5a2a8155c31f1f1bc219047
정찬진 <chanjin@kookmin.ac.kr>
2024-04-28 23:50:52 +0900

    [Gold IV] Title: 인구 이동, Time: 4896 ms, Memory: 31436 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -0,0 +1,43 @@
+import sys
+sys.setrecursionlimit(10000)
+n,l,r = map(int,input().split())
+popul = []
+for _ in range(n):
+    popul.append(list(map(int,input().split())))
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
+#국경선을 열지말지 결정하는 함수
+def dfs(a,b,graph):
+    visitied[a][b]=True
+    for i in range(4):
+        nx = a+dx[i]
+        ny = b+dy[i]
+        if (0<=nx<n) and (0<=ny<n):
+            if (not visitied[nx][ny]) and (l<=(abs(graph[nx][ny] - graph[a][b]))<=r):
+                loca.append((nx,ny))
+                dfs(nx,ny,graph)
+    #국경선을 연후 인구이동
+    return loca
+
+count = 0
+while True:
+    visitied = [[False]*n for _ in range(n)]
+    flag = False 
+    for i in range(n):
+        for j in range(n):
+            loca = [(i,j)]
+            if not visitied[i][j]:
+                loca = dfs(i,j,popul)            
+            if len(loca)>1:
+                flag = True
+                sum = 0
+                for x,y in loca:
+                    sum+=popul[x][y]
+                avg = sum//len(loca)
+                for a,b in loca:
+                    popul[a][b]=int(avg)
+
+    if not flag:
+        print(count)
+        break
+    count+=1
