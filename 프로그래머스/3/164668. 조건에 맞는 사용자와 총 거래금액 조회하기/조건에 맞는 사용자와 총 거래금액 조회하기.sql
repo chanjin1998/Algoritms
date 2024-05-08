00a1f949ccf46dbb95993ed90f095356136f0135
정찬진 <chanjin@kookmin.ac.kr>
2024-05-08 21:44:36 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,8 +1,7 @@
--- 코드를 입력하세요
-#총금액이 70만 원 이상인 사람의 회원 ID, 닉네임, 총거래금액
-SELECT user_id, b.nickname, sum(price) as total_sales from used_goods_board as a
-left join used_goods_user as b on a.writer_id = b.user_id
-where status = 'DONE'
+select user_id, nickname, sum(price) as price
+from used_goods_board as ugb join used_goods_user as ugu 
+on ugb.writer_id = ugu.user_id
+where status = 'done'
 group by user_id
-having total_sales >= 700000
-order by total_sales
+having price >= 700000
+order by price
