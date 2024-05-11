df78cbde2d757bbf99a2fc50c19371f02cdbe364
chanjin1998 <chanjin@kookmin.ac.kr>
2024-05-12 00:00:00 +0900

    temp file

--- ttt.py
+++ ttt.py
@@ -1,22 +1,12 @@
-import math
 import sys
-def four_squares(n):
 
-  if int(math.sqrt(n)) == math.sqrt(n):
-    return 1
+n,k = map(int,sys.stdin.readline().split())
+group = list(map(int,sys.stdin.readline().split()))
 
-  for i in range(1, int(math.sqrt(n)) + 1):
-
-    if int(math.sqrt(n - pow(i, 2))) == math.sqrt(n - pow(i, 2)):
-      return 2
-
-  for i in range(1, int(math.sqrt(n)) + 1):
-        for j in range(1, int(math.sqrt(n - pow(i,2))) + 1):
-            if int(math.sqrt(n - pow(i,2) - pow(j,2))) == math.sqrt(n - pow(i,2) - pow(j,2)):
-                return 3
-
-  return 4
-
-n = int(sys.stdin.readline())
-print(four_squares(n))
-#print(aa)
+tmp = []
+for i in range(n-1):
+  dif = group[i+1] - group[i]
+  tmp.append((i,dif))
+print(tmp)
+tmp = tmp.
+print(tmp)
