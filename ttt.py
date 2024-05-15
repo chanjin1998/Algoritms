dc831ba373edd1e15bcbe6b6be79be8b0cdd6866
chanjin1998 <chanjin@kookmin.ac.kr>
2024-05-16 00:00:00 +0900

    행복유치원 그리디

--- ttt.py
+++ ttt.py
@@ -4,9 +4,10 @@ n,k = map(int,sys.stdin.readline().split())
 group = list(map(int,sys.stdin.readline().split()))
 
 tmp = []
+val = []
 for i in range(n-1):
   dif = group[i+1] - group[i]
-  tmp.append((i,dif))
-print(tmp)
-tmp = tmp.
-print(tmp)
+  tmp.append(dif)
+tmp.sort(reverse=True)
+
+print(sum((tmp[k-1:])))
