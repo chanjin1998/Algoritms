0527dd648b5377bfea7ca50f92cfdd668e86d49d
정찬진 <chanjin@kookmin.ac.kr>
2024-06-18 19:36:59 +0900

    [level 2] Title: 짝지어 제거하기, Time: 181.73 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,23 +1,17 @@
 def solution(s):
-    answer = 0
-    stackS = []
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.    
-    
-    
+    stack = []
     for i in range(len(s)):
-        
-        #스택이 비어있는 경우
-        if len(stackS) == 0:
-            stackS.append(s[i])
-        #제일 뒤에 있는 원소를 비교
-        elif stackS[-1] == s[i]:
-            stackS.pop()
-        #삭제 가능한 문자가 없을 때
+        if i == 0:
+            stack.append(s[i])
+        elif len(stack) != 0:
+            if stack[-1] == s[i]:
+                stack.pop()
+            else:
+                stack.append(s[i])
         else:
-            stackS.append(s[i])
-    
-    #문자열 제거 후 남아있는게 없으면 1 반환
-    if len(stackS) == 0:
+            stack.append(s[i])
+    if len(stack) == 0:
         return 1
-    
-    return answer
+    else:
+        return 0
+        
